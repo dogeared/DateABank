f07dd083b454c5aecf310499fce09d75879e6046
Micah Silverman <micah.silverman@didit.com>
2012-02-15 10:00:42 -0500

    Micah - updated app.js for port and got rid of commented out lines.

--- app.js
+++ app.js
@@ -81,10 +81,9 @@ process.on('uncaughtException', function(err) {
        require(__dirname + '/lib/controllers/' + controller)(app);
      }
    });
-   var port = process.env.PORT || 3000;
+   var port = parseInt(process.env.PORT) || 3000;
    app.listen(port);
    console.log("Express server listening on port %d in %s mode", 
     app.address().port, app.settings.env);
 })
-// require(__dirname + '/lib/controllers/site_controller')(app);
 
